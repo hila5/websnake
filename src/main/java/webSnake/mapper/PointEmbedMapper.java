8acb558bef04c4055bdc6881062f82214adb817f
Hila Cohen <hila.cohen07@gmail.com>
2026-03-18 20:03:06 +0200

    fix: remove componentModel = "spring" and unused function

--- src/main/java/webSnake/mapper/PointEmbedMapper.java
+++ src/main/java/webSnake/mapper/PointEmbedMapper.java
@@ -1,12 +1,11 @@
 package webSnake.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import webSnake.entity.PointEmbed;
 
 import java.awt.*;
 
-@Mapper(componentModel = "spring")
+@Mapper()
 public interface PointEmbedMapper {
 
     default PointEmbed map(Point point) {
@@ -15,14 +14,6 @@ public interface PointEmbedMapper {
 
     default Point map(PointEmbed pointEmbed) {
         return pointEmbed == null ? null : pointEmbed.toPoint();
-    }
-
-    default void updateFromPoint(Point source, @MappingTarget PointEmbed target) {
-        if (target == null) {
-            target = new PointEmbed(source);
-        } else {
-            target.set(source);
-        }
     }
 
 }
