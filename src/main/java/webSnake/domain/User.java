cb12de8255cfdf6d663e47f4644fab030a12be81
Hila Cohen <hila.cohen07@gmail.com>
2026-03-30 18:52:29 +0300

    style: remove unused fields

--- src/main/java/webSnake/domain/User.java
+++ src/main/java/webSnake/domain/User.java
@@ -8,15 +8,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Setter
 @Log4j2
 public class User {
 
-    private Long userId;
     private String username;
     private String creationTime;
-    private String lastPlayed;
-    private List<Game> games;
 
     public User(String username) {
         this.username = username;
