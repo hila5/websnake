f1969af65ac0b583ea6d1128ca9a55182a516b37
Hila Cohen <hila.cohen07@gmail.com>
2026-03-30 22:04:11 +0300

    feat: add bean for UserMapper in the MapperConfig

--- src/main/java/webSnake/config/MapperConfig.java
+++ src/main/java/webSnake/config/MapperConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import webSnake.mapper.GameMapper;
 import webSnake.mapper.PointEmbedMapper;
+import webSnake.mapper.UserMapper;
 
 @Configuration
 public class MapperConfig {
@@ -17,6 +18,11 @@ public class MapperConfig {
     @Bean
     public GameMapper gameMapper() {
         return Mappers.getMapper(GameMapper.class);
+    }
+
+    @Bean
+    public UserMapper userMapper() {
+        return Mappers.getMapper(UserMapper.class);
     }
 
 }
